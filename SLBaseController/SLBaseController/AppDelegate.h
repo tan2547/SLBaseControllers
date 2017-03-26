7af1f7af784b459fc4e47ba11e048ac96a5cb24e
Sylar <254754424@qq.com>
2017-03-26 13:33:10 +0800

    ....

---
+++ SLBaseController/SLBaseController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SLBaseController
+//
+//  Created by 谭春江 on 2017/3/26.
+//  Copyright © 2017年 Sylar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SLBaseController/SLBaseController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SLBaseController
+//
+//  Created by 谭春江 on 2017/3/26.
+//  Copyright © 2017年 Sylar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
