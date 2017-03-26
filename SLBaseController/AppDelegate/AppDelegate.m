4dd5364067030794d551af0fbbbd5d848ffc1477
Sylar <254754424@qq.com>
2017-03-26 13:46:54 +0800

    ...

--- SLBaseController/AppDelegate/AppDelegate.m
+++ SLBaseController/AppDelegate/AppDelegate.m
@@ -17,6 +17,12 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
+    [self.window makeKeyAndVisible];
+
+    UIViewController *vc = [[UIViewController alloc]init];
+    vc.view.backgroundColor = [UIColor whiteColor];
+    [self.window setRootViewController:vc];
     return YES;
 }
 
